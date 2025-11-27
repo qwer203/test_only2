153d38d34c3694df8e74d238dde69069a829871a
polina <bolotovapolinushka@yandex.ru>
2025-11-27 14:20:08 +0300

    edits

---
+++ components/bitrix/news.list/template1/template.php
@@ -0,0 +1,55 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+$this->setFrameMode(true);
+?>
+
+<div id="barba-wrapper">
+    <div class="article-list">
+        <?if($arParams["DISPLAY_TOP_PAGER"]):?>
+            <?=$arResult["NAV_STRING"]?><br />
+        <?endif;?>
+        
+        <?foreach($arResult["ITEMS"] as $arItem):?>
+            <?php
+            $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+            $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+            ?>
+            
+            <a class="article-item article-list__item" 
+               href="<?=$arItem["DETAIL_PAGE_URL"]?>" 
+               data-anim="anim-3"
+               id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+                
+                <div class="article-item__background">
+                    <?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
+                        <img src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
+                             data-src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
+                             alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
+                             title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"/>
+                    <?else:?>
+                        <img src="/images/no-photo.jpg" alt="Нет изображения"/>
+                    <?endif;?>
+                </div>
+                
+                <div class="article-item__wrapper">
+                    <?if($arParams["DISPLAY_NAME"]!="N" && $arItem["NAME"]):?>
+                        <div class="article-item__title"><?=$arItem["NAME"]?></div>
+                    <?endif;?>
+                    
+                    <?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arItem["PREVIEW_TEXT"]):?>
+                        <div class="article-item__content"><?=$arItem["PREVIEW_TEXT"]?></div>
+                    <?endif;?>
+                </div>
+            </a>
+        <?endforeach;?>
+        
+        <?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
+            <br /><?=$arResult["NAV_STRING"]?>
+        <?endif;?>
+    </div>
+</div>
